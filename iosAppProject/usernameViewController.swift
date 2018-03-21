bfc039bf4c29c6888e11714247ac61989257a1b0
José Luis  Enríquez Flores <jlenriquez@Joses-MacBook-Air.local>
2018-03-21 10:43:30 -0600

    loginscreen

---
+++ iosAppProject/usernameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  usernameViewController.swift
+//  iosAppProject
+//
+//  Created by José Luis  Enríquez Flores on 3/21/18.
+//  Copyright © 2018 Baikio. All rights reserved.
+//
+
+import UIKit
+
+class usernameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
